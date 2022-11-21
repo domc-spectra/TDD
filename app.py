1759196cde59835ae9459f5d6c578910f58d782e
Dom Caracappa <dom.c@spectrawatermakers.com>
2022-11-21 09:24:47 -0800

    Hello World

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def Home(name=None):
+    return render_template('base.html')
+
+
+# debug mode running on 5050 port
+if __name__=="__main__":
+    app.run(host="localhost", debug=True, port=5050)
